862fb4693c1cd7c572862c1c70e3a37d2058670b
luis <techstuff.tweezers191@aleeas.com>
2025-03-15 10:42:21 +0100

    Organise the config file
    - Seperate in general settings and paths

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 from game_utils.direction import Direction
 
-# General settings
+####################
+# General settings #
+####################
+
 FPS = 50
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 400
@@ -13,15 +16,23 @@ FIELD_COLOR = (200, 220, 150)
 BG_COLOR = (50, 50, 70)
 TEXT_COLOR = (0, 0, 0)
 
-# Images
-ROOT_DIR = Path(__file__).resolve().parent # Gets the directory of the current script
-TOMATO_IMAGE_PATH = ROOT_DIR / "assets" / "images" / "apple.png"
-
-# Fonts
-PIXEL_FONT_PATH = ROOT_DIR / "assets" / "fonts" / "PixelifySans-VariableFont_wght.ttf"
 
 # Game starting position
 SNAKE_DEFAULT_DIRECTION = Direction.EAST
 def calc_starting_position(field_width, field_height):
     return ((field_width // 4, field_height // 2),
             (field_width // 4 - 1, field_height // 2))
+
+#########
+# Paths #
+#########
+ROOT_DIR = Path(__file__).resolve().parent # Gets the directory of the current script
+
+# Images
+TOMATO_IMAGE_PATH = ROOT_DIR / "assets" / "images" / "apple.png"
+
+# Fonts
+PIXEL_FONT_PATH = ROOT_DIR / "assets" / "fonts" / "PixelifySans-VariableFont_wght.ttf"
+
+# AI model
+SAVE_MODEL_PATH = ROOT_DIR / "ai" / "models"
